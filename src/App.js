cf59bf3b6f654a11027b61419bb36c99c28e5955
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-25 07:18:52 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            <Header></Header>
+            <Shop></Shop>
+        </div>
+    );
 }
 
 export default App;
